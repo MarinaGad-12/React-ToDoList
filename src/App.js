7175a681de30e7a6b1bc6ff6582e03d52e07690f
marin@LAPTOP-MGTFGKFP <marinagad996@gmail.com>
2021-12-10 23:51:29 +0200

    ToDo-List

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Todolist from "./views/todolist";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Todolist />
+    </>
+  );
+}
+
+export default App;
